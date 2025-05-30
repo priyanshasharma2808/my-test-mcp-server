99cc117eed0eed53bc6c2214972de789383c2388
Priyansha Sharma <priysharma@microsoft.com>
2025-05-30 16:10:29 +0530

    added sse code

--- index.js
+++ index.js
@@ -1,11 +1,81 @@
+// import { McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
+// import { StreamableHTTPServerTransport  } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+// import { z } from 'zod';
+// import express from 'express';
+// import bodyParser from 'body-parser';
+
+// const app = express();
+// app.use(bodyParser.json());
+// // Create an MCP server
+// const server = new McpServer({
+//   name: "Weather data fetcher",
+//   version: "1.0.0"
+// });
+// async function getWeatherDataByCityName(city = '') {
+//     if(city.toLowerCase() == 'patiala') {
+//         return { Temp: '30C', Forcast: 'rain' };
+//     }
+//     else if(city.toLowerCase() == 'delhi') {
+//         return { Temp: '40C', Forcast: 'wind' };
+//     }
+//     else if(city.toLowerCase() == 'mumbai') {
+//         return { Temp: '35C', Forcast: 'Bohot barish ho rhi hai idhar' };
+//     }
+//     else if(city.toLowerCase() == 'chandigarh') {
+//         return { Temp: '32C', Forcast: 'sunny' };   
+//     }
+//     else {
+//         return { Temp: null, Forcast: 'unable to get data' };
+//     }
+// }
+// server.tool('getWeatherDataByCityName', {
+//     city: z.string()
+// }, async ({ city }) => {
+//     return { content: [{ type: "text", text: JSON.stringify(await getWeatherDataByCityName(city)) }] };
+// }
+// );
+
+// let transport = null;
+
+// app.get('/stream', (req, res) => {
+//   transport = new StreamableHTTPServerTransport("/messages", res);
+//   server.connect(transport);
+// });
+
+// app.post('/messages', (req, res) => {
+//   if (transport) {
+//     transport.handlePostMessage(req, res);
+//   } else {
+//     res.status(400).json({ error: 'Transport not initialized.' });
+//   }
+// });
+
+// // app.get('/stream', async (req, res) => {
+// //   const city = req.query.city;
+
+// //   if (!city) {
+// //     res.status(400).json({ error: 'City query parameter is required.' });
+// //     return;
+// //   }
+
+// //   try {
+// //     const data = await getWeatherDataByCityName(city);
+// //     res.setHeader('Content-Type', 'application/json');
+// //     res.write(JSON.stringify({ city, data }));
+// //     res.end();
+// //   } catch (error) {
+// //     res.status(500).json({ error: 'Internal Server Error' });
+// //   }
+// // });
+
+// app.listen(3000);
+
+
 import { McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StreamableHTTPServerTransport  } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
 import { z } from 'zod';
 import express from 'express';
-import bodyParser from 'body-parser';
-
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
 const app = express();
-app.use(bodyParser.json());
 // Create an MCP server
 const server = new McpServer({
   name: "Weather data fetcher",
@@ -21,9 +91,6 @@ async function getWeatherDataByCityName(city = '') {
     else if(city.toLowerCase() == 'mumbai') {
         return { Temp: '35C', Forcast: 'Bohot barish ho rhi hai idhar' };
     }
-    else if(city.toLowerCase() == 'chandigarh') {
-        return { Temp: '32C', Forcast: 'sunny' };   
-    }
     else {
         return { Temp: null, Forcast: 'unable to get data' };
     }
@@ -34,38 +101,15 @@ server.tool('getWeatherDataByCityName', {
     return { content: [{ type: "text", text: JSON.stringify(await getWeatherDataByCityName(city)) }] };
 }
 );
-
 let transport = null;
-
-app.get('/stream', (req, res) => {
-  transport = new StreamableHTTPServerTransport("/messages", res);
-  server.connect(transport);
+app.get('/sse', (req, res) => {
+    transport = new SSEServerTransport("/messages", res);
+    server.connect(transport);
 });
-
 app.post('/messages', (req, res) => {
-  if (transport) {
-    transport.handlePostMessage(req, res);
-  } else {
-    res.status(400).json({ error: 'Transport not initialized.' });
-  }
+    if (transport) {
+        transport.handlePostMessage(req, res);
+    }
 });
-
-// app.get('/stream', async (req, res) => {
-//   const city = req.query.city;
-
-//   if (!city) {
-//     res.status(400).json({ error: 'City query parameter is required.' });
-//     return;
-//   }
-
-//   try {
-//     const data = await getWeatherDataByCityName(city);
-//     res.setHeader('Content-Type', 'application/json');
-//     res.write(JSON.stringify({ city, data }));
-//     res.end();
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
 app.listen(3000);
+
